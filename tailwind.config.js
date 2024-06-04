85efc135e563ff330fc9a6146752be17e451e10b
Lucas <keyruu@web.de>
2024-06-04 20:28:30 +0200

    more stuff pretty cool

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
+const { addIconSelectors } = require('@iconify/tailwind');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.templ"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    addIconSelectors(['simple-icons']),
+  ],
   safelist: [
     'left-0', 'left-full', 'right-0', 'right-full', 'before:left-0', 'before:left-full', 'before:right-0', 'before:right-full'
   ]
