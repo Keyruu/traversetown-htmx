6c177fde09ad02f6248b51b8bb3eadeaa875cd6c
Lucas <keyruu@web.de>
2024-06-24 12:00:38 +0200

    fix issues with releases

--- utils/templ.go
+++ utils/templ.go
@@ -2,12 +2,18 @@ package utils
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/keyruu/traversetown-htmx/config"
+	"github.com/keyruu/traversetown-htmx/models"
 )
 
 func GetEnv(ctx context.Context) *config.Env {
 	return ctx.Value(EnvContextKey).(*config.Env)
+}
+
+func ReleasesImagePath(release models.Releases) string {
+	return fmt.Sprintf("/api/files/releases/%s/%s", release.Id, release.Cover)
 }
 
 type InfoBoxIcon struct {
